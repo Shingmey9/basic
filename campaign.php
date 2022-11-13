0534ee9b2073f7967194e6c1199b1ce37325c50c
Shingmey9 <110320215+Shingmey9@users.noreply.github.com>
2022-11-13 09:15:12 +0600

    Add files via upload

---
+++ campaign.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="campaign.css">
+
+
+</head>
+<body>
+    <div class="main">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="https://www.lstream.org/wp-content/uploads/cropped-ALLWHITELifeStream-Blood-Bank-Logo-Stacked-Rev.png "alt="logo">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="campaign.php">Campaign</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="donate.php">Donate</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="needblood.php">Need Blood</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                      </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <div class="row">
+              <div class="col align-self-start">
+                
+              </div>
+              <div class="col align-self-center">
+                <div class="content">
+                    <h5>UNIVERSITY OF THE BANGLADESH OPEN UNIVERSITY </h5>
+                    <h1>2022 BLOOD DONATION CAMPAIGN</h1>
+                    <h4>Give blood. Help save a life.</h4>
+                    <p class="para1">Start Date: 09 November 2022| 9.00AM<br> End Date: 15 November 2022| 5.00PM</p>
+                    <p class="para2">Location: Government Laboratory School Road, Dhanmondi, Dhaka-1205, Bangladesh</p>
+                </div> 
+              </div>
+              <div class="col align-self-end">
+                
+              </div>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
+
